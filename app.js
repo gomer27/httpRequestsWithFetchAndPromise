1b1e9f8004a87bf18a817de246f5e7ea9bcc05b0
Ömer Güneş <omergunes2700@gmail.com>
2022-12-23 02:42:39 +0300

    fetch ve promise ile Http requesti - Get Requesti

---
+++ app.js
@@ -0,0 +1,20 @@
+// Get Requesti
+class Request {
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+}
+const request = new Request();
+let albums;
+request
+  .get("https://jsonplaceholder.typicode.com/albums")
+  .then((albums) => {
+    console.log(albums); // sekron
+  })
+  .catch((err) => console.log(err));
+//console.log(albums); //Asekron
