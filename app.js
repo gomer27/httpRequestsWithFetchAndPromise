65817d461aec8170f8bbb813b8706d2b30c1c95c
Ömer Güneş <omergunes2700@gmail.com>
2022-12-23 03:02:56 +0300

    fetch ve promise ile Http requesti - Post Requesti

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// Get Requesti
 class Request {
+  // Get Requesti
   get(url) {
     return new Promise((resolve, reject) => {
       fetch(url)
@@ -8,13 +8,41 @@ class Request {
         .catch((err) => reject(err));
     });
   }
+  // Post Requesti
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
 }
+
 const request = new Request();
-let albums;
-request
-  .get("https://jsonplaceholder.typicode.com/albums")
-  .then((albums) => {
-    console.log(albums); // sekron
-  })
-  .catch((err) => console.log(err));
+
+// Get Request
+
+// let albums;
+// request
+//   .get("https://jsonplaceholder.typicode.com/albums")
+//   .then((albums) => {
+//     console.log(albums); // sekron
+//   })
+//   .catch((err) => console.log(err));
 //console.log(albums); //Asekron
+
+// Post Request
+request
+  .post("https://jsonplaceholder.typicode.com/albums", {
+    userId: 1,
+    title: "Naber",
+  })
+  .then((newAlbum) => console.log(newAlbum))
+  .catch((err) => console.log(err));
